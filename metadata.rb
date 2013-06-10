74b76d410cc932da37b824740e9f31bf38317603
Caleb Groom <cgroom@rackspace.com>
2013-06-10 14:28:33 -0500

    Add metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name              "setupvenv"
+maintainer        "Caleb Groom"
+maintainer_email  "caleb@calebgroom.com"
+license           "Apache 2.0"
+description       "Cookbook that creates a Python virtual environment and activates it on user login"
+version           "0.0.1"
+depends           "python"

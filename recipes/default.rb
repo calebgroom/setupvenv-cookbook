9852ece0fe8e1e7115f6dd9ec5101b5bf6f30232
Caleb Groom <cgroom@rackspace.com>
2013-06-10 15:11:42 -0500

    `template` not `template_file`

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,7 +4,7 @@ python_virtualenv node["setupvenv"]["dir"] do
   action :create
 end
 
-template_file "#{node[:setupvenv][:dir]}/.bash_profile" do
+template "#{node[:setupvenv][:dir]}/.bash_profile" do
   source "bash_profile.erb"
   owner node["setupvenv"]["user"]
   group node["setupvenv"]["group"]
